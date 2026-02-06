784802672734558485ece6c5f1d3bae805c92de1
clyde <clyde.salar@rocketpartners.io>
2026-02-06 15:56:01 +0800

    Remove TanStack DevTools completely and fix SPA routing on refresh

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './components/ThemeProvider'
 import App from './App'
@@ -23,7 +22,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
           <App />
         </BrowserRouter>
-        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
       </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
